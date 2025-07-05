0d222f82de7059aa09baf8959b8a8dcdcebd0bec
Ilia <latariailia6@gmail.com>
2025-07-05 15:00:19 -0700

    update task

--- order-processing-service/src/main/java/com/example/orderprocessingservice/OrderProcessingServiceApplication.java
+++ order-processing-service/src/main/java/com/example/orderprocessingservice/OrderProcessingServiceApplication.java
@@ -17,11 +17,11 @@ public class OrderProcessingServiceApplication {
         2. Email service to send email to customer on order receive on 15 minutes away and on door
         3. Introduce AI drivers that might pick up orders from the pool on their own
         4. Cleanup code
-        5. Page of graphs about tables, like wareHouses, products, customers, employees to wareHouses, etc.,
-        6. If we cannot get from all wareHouses that product amount, then we just get whatever we can
+        5. If we cannot get from all wareHouses that product amount, then we just get whatever we can
         and move that order or implement some logic to move to a wait list, and when wareHouse gets that product,
         it automatically gets that again and goes like so.
-
+        6. We should send to "graphs" customer, employee, product, supplier, warehouse tables for now.
+        first when we load to database tables with data, we should send that as init data and then on each call
 
      */
 }
