57a45939a90df972f547c3c357fd10c2a058645a
Ilia <latariailia6@gmail.com>
2025-07-10 15:11:48 -0700

    modified order-processing-service/ Database loaders

---
+++ order-processing-service/src/main/java/com/example/orderprocessingservice/utils/constants/UrlConstants.java
@@ -0,0 +1,12 @@
+package com.example.orderprocessingservice.utils.constants;
+
+public class UrlConstants {
+    private static final String BASE_URL = "http://localhost:8081/api/v1/";
+
+    public static final String WAREHOUSE_POST_ENDPOINT = BASE_URL + "warehouse";
+    public static final String PRODUCT_POST_ENDPOINT = BASE_URL + "product";
+    public static final String CUSTOMER_POST_ENDPOINT = BASE_URL + "customer";
+    public static final String SUPPLIER_POST_ENDPOINT = BASE_URL + "supplier";
+    public static final String EMPLOYEE_POST_ENDPOINT = BASE_URL + "employee";
+    public static final String STOCK_POST_ENDPOINT = BASE_URL + "stock";
+}
