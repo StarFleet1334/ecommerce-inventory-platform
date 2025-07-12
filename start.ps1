a97d5cce891eb8f1a3c4bc245a32d31616382956
Ilia <latariailia6@gmail.com>
2025-07-12 16:09:20 -0700

    update start.ps1 file

--- start.ps1
+++ start.ps1
@@ -4,25 +4,10 @@ Remove-Item -Path "rocketmq/data/broker/store/*" -Force -Recurse -ErrorAction Si
 Remove-Item -Path "rocketmq/data/namesrv/logs/rocketmqlogs/*" -Force -Recurse -ErrorAction SilentlyContinue
 Remove-Item -Path "rocketmq/data/namesrv-logs/rocketmqlogs/*" -Force -Recurse -ErrorAction SilentlyContinue
 
-Write-Host "Starting namesrv..."
-docker compose up -d --build namesrv
-
-Write-Host "Waiting for namesrv to be healthy..."
+Write-Host "Starting services"
+docker-compose --profile dev up --build
 Start-Sleep -Seconds 10
 
-Write-Host "Starting broker..."
-docker compose up -d --build broker
 
-Write-Host "Waiting for broker to be healthy..."
-Start-Sleep -Seconds 15
-
-Write-Host "Starting dashboard..."
-docker compose up -d --build dashboard
-
-Write-Host "Waiting for dashboard to be healthy..."
-Start-Sleep -Seconds 10
-
-Write-Host "Starting RocketMQ-Service..."
-docker compose up -d --build rocketmq-service
 
 Write-Host "RocketMQ services are starting. You can check logs with 'docker compose logs -f'"
