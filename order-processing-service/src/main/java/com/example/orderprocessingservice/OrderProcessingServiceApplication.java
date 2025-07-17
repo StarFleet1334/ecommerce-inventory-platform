4665b9ce8eea4e36256d96cc46cdc213f3f196ea
Ilia <latariailia6@gmail.com>
2025-07-17 15:03:31 -0700

    implement customer_inventory, fixed starts.ps1 on Windows & resolved order-processing pending... bug

--- order-processing-service/src/main/java/com/example/orderprocessingservice/OrderProcessingServiceApplication.java
+++ order-processing-service/src/main/java/com/example/orderprocessingservice/OrderProcessingServiceApplication.java
@@ -10,17 +10,4 @@ public class OrderProcessingServiceApplication {
         SpringApplication.run(OrderProcessingServiceApplication.class, args);
     }
 
-    /*
-                TASKS:
-        1. Redis Caching
-        2. Email service to send email to customer on order receive on 15 minutes away and on door
-        3. Introduce AI drivers that might pick up orders from the pool on their own
-        4. Cleanup code
-        5. If we cannot get from all wareHouses that product amount, then we just get whatever we can
-        and move that order or implement some logic to move to a wait list, and when wareHouse gets that product,
-        it automatically gets that again and goes like so.
-        6. We should send to "graphs" customer, employee, product, supplier, warehouse tables for now.
-        first when we load to database tables with data, we should send that as init data and then on each call
-
-     */
 }
