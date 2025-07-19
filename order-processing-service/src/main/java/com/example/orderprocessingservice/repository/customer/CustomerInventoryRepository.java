00df6d200983e6ae14d3eb70d09dcd2613ae912d
Ilia <latariailia6@gmail.com>
2025-07-19 14:18:03 -0700

    Update DELETE customer -> transaction -> order -> customer inventory

--- order-processing-service/src/main/java/com/example/orderprocessingservice/repository/customer/CustomerInventoryRepository.java
+++ order-processing-service/src/main/java/com/example/orderprocessingservice/repository/customer/CustomerInventoryRepository.java
@@ -2,11 +2,15 @@ package com.example.orderprocessingservice.repository.customer;
 
 import com.example.orderprocessingservice.dto.model.customer.CustomerInventory;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface CustomerInventoryRepository extends JpaRepository<CustomerInventory, Integer> {
     boolean existsByCustomerIdAndProductId(Integer customerId, String productId);
 
     Optional<CustomerInventory> findByCustomerIdAndProductId(Integer customerId, String productId);
+
+    List<CustomerInventory> findAllByCustomerId(Integer customerId);
 
 }
