ba4956831b760718beed89e15832867ec1e3729b
Ilia <latariailia6@gmail.com>
2025-07-20 13:25:18 -0700

    add STAGE 4 and STAGE 5 of SCRUM-13 ticket

--- order-processing-service/src/main/java/com/example/orderprocessingservice/repository/personnel/WareHouseInventoryBacklogRepository.java
+++ order-processing-service/src/main/java/com/example/orderprocessingservice/repository/personnel/WareHouseInventoryBacklogRepository.java
@@ -2,6 +2,13 @@ package com.example.orderprocessingservice.repository.personnel;
 
 import com.example.orderprocessingservice.dto.model.personnel.WareHouseInventoryBacklog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface WareHouseInventoryBacklogRepository extends JpaRepository<WareHouseInventoryBacklog, Integer> {
+
+    @Query("SELECT w FROM WareHouseInventoryBacklog w WHERE w.wareHouse.wareHouseId = :wId AND w.product.product_id = :pId")
+    List<WareHouseInventoryBacklog> findAllByWareHouseId_ProductId(@Param("wId") int wId, @Param("pId") String pId);
 }
