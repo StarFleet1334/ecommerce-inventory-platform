87f527fa5664ba5d691845c0bdda179820afc837
Ilia <latariailia6@gmail.com>
2025-07-20 13:30:25 -0700

    DELETE wareHouseInventoryBacklogHistory

--- order-processing-service/src/main/java/com/example/orderprocessingservice/repository/personnel/WareHouseInventoryHistoryBacklogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.orderprocessingservice.repository.personnel;
-
-import com.example.orderprocessingservice.dto.model.personnel.WareHouseInventoryBacklogHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WareHouseInventoryHistoryBacklogRepository extends JpaRepository<WareHouseInventoryBacklogHistory,Integer> {
-}
