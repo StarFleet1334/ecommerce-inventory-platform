e06dfc040b0f1d33513e2e30c06e8217dc3b1e1a
Ilia <latariailia6@gmail.com>
2025-07-22 15:13:48 -0700

    update 'DynamicRewriteRoute', refactore HttpRequestMethod naming to principles & updated .json file in /static

--- api-gateway/src/main/java/org/example/apigateway/config/DynamicRewriteRoute.java
+++ api-gateway/src/main/java/org/example/apigateway/config/DynamicRewriteRoute.java
@@ -9,6 +9,8 @@ import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
 import java.util.function.Function;
 
 @Configuration
@@ -35,35 +37,36 @@ public class DynamicRewriteRoute {
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(generateIdentifier("customer",secondBackendName),createRewriteRoute("customer",generateInboundPath(HttpRequestTypes.GET.getName(),"customer"),secondaryBackendUri))
-                .route(generateIdentifier("employee",secondBackendName),createRewriteRoute("employee",generateInboundPath(HttpRequestTypes.GET.getName(),"employee"),secondaryBackendUri))
-                .route(generateIdentifier("warehouse",secondBackendName),createRewriteRoute("warehouse",generateInboundPath(HttpRequestTypes.GET.getName(),"warehouse"),secondaryBackendUri))
-                .route(generateIdentifier("supplier",secondBackendName),createRewriteRoute("supplier",generateInboundPath(HttpRequestTypes.GET.getName(),"supplier"),secondaryBackendUri))
-                .route(generateIdentifier("product",secondBackendName),createRewriteRoute("product",generateInboundPath(HttpRequestTypes.GET.getName(),"product"),secondaryBackendUri))
-                .route(generateIdentifier("supply",primaryBackendName),createRewriteRoute("supply",generateInboundPath(HttpRequestTypes.POST.getName(),"supply"),primaryBackendUri))
-                .route(generateIdentifier("supply",primaryBackendName),createRewriteRoute("supply",generateInboundPath(HttpRequestTypes.DELETE.getName(),"supply"),primaryBackendUri))
-                .route(generateIdentifier("stock",primaryBackendName),createRewriteRoute("stock",generateInboundPath(HttpRequestTypes.POST.getName(),"stock"),primaryBackendUri))
-                .route(generateIdentifier("stock",primaryBackendName),createRewriteRoute("stock",generateInboundPath(HttpRequestTypes.DELETE.getName(),"stock"),primaryBackendUri))
-                .route(generateIdentifier("customer",primaryBackendName),createRewriteRoute("customer",generateInboundPath(HttpRequestTypes.POST.getName(),"customer"),primaryBackendUri))
-                .route(generateIdentifier("customer",primaryBackendName),createRewriteRoute("customer",generateInboundPath(HttpRequestTypes.DELETE.getName(),"customer"),primaryBackendUri))
-                .route(generateIdentifier("customer",primaryBackendName),createRewriteRoute("customer/order",generateInboundPath(HttpRequestTypes.POST.getName(),"customer-order"),primaryBackendUri))
-                .route(generateIdentifier("warehouse",primaryBackendName),createRewriteRoute("warehouse",generateInboundPath(HttpRequestTypes.POST.getName(),"warehouse"),primaryBackendUri))
-                .route(generateIdentifier("warehouse",primaryBackendName),createRewriteRoute("warehouse",generateInboundPath(HttpRequestTypes.DELETE.getName(),"warehouse"),primaryBackendUri))
-                .route(generateIdentifier("supplier",primaryBackendName),createRewriteRoute("supplier",generateInboundPath(HttpRequestTypes.POST.getName(),"supplier"),primaryBackendUri))
-                .route(generateIdentifier("supplier",primaryBackendName),createRewriteRoute("supplier",generateInboundPath(HttpRequestTypes.DELETE.getName(),"supplier"),primaryBackendUri))
-                .route(generateIdentifier("product",primaryBackendName),createRewriteRoute("product",generateInboundPath(HttpRequestTypes.POST.getName(),"product"),primaryBackendUri))
-                .route(generateIdentifier("product",primaryBackendName),createRewriteRoute("product",generateInboundPath(HttpRequestTypes.DELETE.getName(),"product"),primaryBackendUri))
-                .route(generateIdentifier("employee",primaryBackendName),createRewriteRoute("employee",generateInboundPath(HttpRequestTypes.POST.getName(),"employee"),primaryBackendUri))
-                .route(generateIdentifier("employee",primaryBackendName),createRewriteRoute("employee",generateInboundPath(HttpRequestTypes.DELETE.getName(),"employee"),primaryBackendUri))
+                .route(generateIdentifier("customer",secondBackendName),createRewriteRoute("customer",generateInboundPath("customer"),secondaryBackendUri,HttpRequestTypes.GET.getName()))
+                .route(generateIdentifier("employee",secondBackendName),createRewriteRoute("employee",generateInboundPath("employee"),secondaryBackendUri,HttpRequestTypes.GET.getName()))
+                .route(generateIdentifier("warehouse",secondBackendName),createRewriteRoute("warehouse",generateInboundPath("warehouse"),secondaryBackendUri,HttpRequestTypes.GET.getName()))
+                .route(generateIdentifier("supplier",secondBackendName),createRewriteRoute("supplier",generateInboundPath("supplier"),secondaryBackendUri,HttpRequestTypes.GET.getName()))
+                .route(generateIdentifier("product",secondBackendName),createRewriteRoute("product",generateInboundPath("product"),secondaryBackendUri,HttpRequestTypes.GET.getName()))
+                .route(generateIdentifier("supply",primaryBackendName),createRewriteRoute("supply",generateInboundPath("supply"),primaryBackendUri,HttpRequestTypes.POST.getName()))
+                .route(generateIdentifier("supply",primaryBackendName),createRewriteRoute("supply",generateInboundPath("supply"),primaryBackendUri,HttpRequestTypes.DELETE.getName()))
+                .route(generateIdentifier("stock",primaryBackendName),createRewriteRoute("stock",generateInboundPath("stock"),primaryBackendUri,HttpRequestTypes.POST.getName()))
+                .route(generateIdentifier("stock",primaryBackendName),createRewriteRoute("stock",generateInboundPath("stock"),primaryBackendUri,HttpRequestTypes.DELETE.getName()))
+                .route(generateIdentifier("customer",primaryBackendName),createRewriteRoute("customer",generateInboundPath("customer"),primaryBackendUri,HttpRequestTypes.POST.getName()))
+                .route(generateIdentifier("customer",primaryBackendName),createRewriteRoute("customer",generateInboundPath("customer"),primaryBackendUri,HttpRequestTypes.DELETE.getName()))
+                .route(generateIdentifier("customer",primaryBackendName),createRewriteRoute("customer/order",generateInboundPath("customer-order"),primaryBackendUri,HttpRequestTypes.POST.getName()))
+                .route(generateIdentifier("warehouse",primaryBackendName),createRewriteRoute("warehouse",generateInboundPath("warehouse"),primaryBackendUri,HttpRequestTypes.POST.getName()))
+                .route(generateIdentifier("warehouse",primaryBackendName),createRewriteRoute("warehouse",generateInboundPath("warehouse"),primaryBackendUri,HttpRequestTypes.DELETE.getName()))
+                .route(generateIdentifier("supplier",primaryBackendName),createRewriteRoute("supplier",generateInboundPath("supplier"),primaryBackendUri,HttpRequestTypes.POST.getName()))
+                .route(generateIdentifier("supplier",primaryBackendName),createRewriteRoute("supplier",generateInboundPath("supplier"),primaryBackendUri,HttpRequestTypes.DELETE.getName()))
+                .route(generateIdentifier("product",primaryBackendName),createRewriteRoute("product",generateInboundPath("product"),primaryBackendUri,HttpRequestTypes.POST.getName()))
+                .route(generateIdentifier("product",primaryBackendName),createRewriteRoute("product",generateInboundPath("product"),primaryBackendUri,HttpRequestTypes.DELETE.getName()))
+                .route(generateIdentifier("employee",primaryBackendName),createRewriteRoute("employee",generateInboundPath("employee"),primaryBackendUri,HttpRequestTypes.POST.getName()))
+                .route(generateIdentifier("employee",primaryBackendName),createRewriteRoute("employee",generateInboundPath("employee"),primaryBackendUri,HttpRequestTypes.DELETE.getName()))
                 .build();
     }
 
     private Function<PredicateSpec, Buildable<Route>> createRewriteRoute(
-            String resource,String inbound,String backendUri) {
-
+            String resource,String inbound,String backendUri,String httpRequestMethod) {
         String backend = streamURIPrefix + resource;
 
         return r -> r
+                .method(HttpMethod.valueOf(httpRequestMethod))
+                .and()
                 .path(inbound, inbound + "/**")
                 .filters(f -> f.rewritePath(inbound + "(?<segment>/?.*)",
                         backend + "${segment}"))
@@ -74,8 +77,8 @@ public class DynamicRewriteRoute {
         return String.format("%s-get-%s", backendName, resource);
     }
 
-    private String generateInboundPath(String httpRequestType,String resource) {
-        return String.format("/%s-%s", httpRequestType, resource);
+    private String generateInboundPath(String resource) {
+        return "/" + resource;
     }
 
 }
